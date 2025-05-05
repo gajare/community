5d168fc236b9d98d3ac4cf9bde7ead80a3e84634
gajare <gajare147@gmail.com>
2025-05-06 00:10:14 +0530

    all in one

--- procore_logs/call_logs/backend/main.go
+++ procore_logs/call_logs/backend/main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	// Load environment variables
-	err := godotenv.Load("../.env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
